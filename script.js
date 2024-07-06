36e267b9fab921b4a41228f9926c257b8b073323
kihan2518B <kp648027@gmail.com>
2024-07-06 21:35:40 +0530

    deals and story section responsive

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const scroll = new LocomotiveScroll({
+//         el: document.querySelector(".deals-section"),
+//         smooth: true,
+//         direction: 'horizontal'
+//     });
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const container = document.querySelector('.deals-container');
+//     Scrollbar.init(container, { 
+//         damping: 0.1, 
+//         alwaysShowTracks: true 
+//     });
+// });
+
 const links = document.querySelectorAll('nav ul li a');
 
 links.forEach(link => {
